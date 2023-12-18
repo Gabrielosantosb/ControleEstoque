48d8f5bc2cb9c181d5cc1bedb83debfce8c3e174
gabrielosantosb <email@email.com>
2023-12-18 15:13:37 -0300

    Criando cards cadastro

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  loginCard = true;
+
+  signUpForm = this.formBuilder.group({
+    name: ["", Validators.required],
+    email: ["", Validators.required],
+    password: ["", Validators.required]
+  })
+  loginForm = this.formBuilder.group({
+    email: ["", Validators.required],
+    password: ["", Validators.required]
+  })
+  constructor(private formBuilder: FormBuilder){}
+
+  onSubmitLoginForm(): void{
+    console.log("Dados:", this.loginForm.value)
+  }
+}
