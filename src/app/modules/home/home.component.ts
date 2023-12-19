0814fce774acd024766414eb350907cd91a71dec
gabrielosantosb <email@email.com>
2023-12-19 11:44:08 -0300

    Atribuindo funcoes cadastro e login

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
+import { UserService } from "../../services/user/user.service";
+import { AuthRequest } from "../../../models/interfaces/auth/AuthRequest";
+import { SignUpUserRequest } from "../../../models/interfaces/user/SignUpUserRequest";
+import { tap } from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -8,23 +12,64 @@ import {FormBuilder, Validators} from "@angular/forms";
 })
 export class HomeComponent {
   loginCard = true;
+  errorMessage = "";
+
+  loginForm = this.formBuilder.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", Validators.required]
+  });
 
   signUpForm = this.formBuilder.group({
     name: ["", Validators.required],
-    email: ["", Validators.required],
-    password: ["", Validators.required]
-  })
-  loginForm = this.formBuilder.group({
-    email: ["", Validators.required],
-    password: ["", Validators.required]
-  })
-  constructor(private formBuilder: FormBuilder){}
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(6)]]
+  });
 
-  onSubmitLogin(): void{
-    console.log("Dados:", this.loginForm.value)
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {}
+
+  clearErrorMessage(): void {
+    this.errorMessage = "";
+  }
+  onSubmitLogin(): void {
+
+    if (this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest).pipe(
+        tap(
+          (response) => {
+            if (response) {
+              this.errorMessage = "";
+              alert("Usuário logado");
+            }
+          }
+        )
+      ).subscribe({
+        error: (err) => {
+          this.errorMessage = "Usuário Inválido";
+          console.log(err);
+        }
+      });
+    } else {
+      this.errorMessage = "Por favor, corrija os campos destacados.";
+    }
   }
 
-  onSubmitSignUp(): void{
-    console.log("Dados:", this.signUpForm.value)
+  onSubmitSignUp(): void {
+    if (this.signUpForm.valid) {
+      this.userService.signupUser(this.signUpForm.value as SignUpUserRequest).pipe(
+        tap(
+          (response) => {
+            if (response) {
+              alert("Usuário criado com sucesso");
+            }
+          }
+        )
+      ).subscribe({
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    } else {
+      this.errorMessage = "Por favor, corrija os campos destacados.";
+    }
   }
 }
