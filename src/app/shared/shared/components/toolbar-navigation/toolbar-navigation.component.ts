b20b88fcec7d5844bf8266f07a5d8d58669c890e
gabrielosantosb <email@email.com>
2023-12-19 16:24:38 -0300

    Funcao logout

--- src/app/shared/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+import {Router} from "@angular/router";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-toolbar-navigation',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class ToolbarNavigationComponent {
+  constructor(private cookie: CookieService, private router: Router, private messageService: MessageService) {}
 
+  logout():void{
+    this.cookie.delete("USER_INFO")
+    void this.router.navigate(["/home"])
+    this.messageService.add({
+      severity: "success",
+      summary: "Logout bem-sucedido",
+      detail: "Você foi desconectado com sucesso do sistema.",
+      life: 2000
+    })
+  }
 }
