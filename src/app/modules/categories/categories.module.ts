1fbe786c911613a69ce07fea7d5d7a1c94c74b83
gabrielosantosb <email@email.com>
2024-01-09 16:25:18 -0300

    adicionando componentes

---
+++ src/app/modules/categories/categories.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesHomeComponent } from './page/categories-home/categories-home.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {CATEGORIES_ROTES} from "./categories.routing";
+import {SharedModule} from "../../shared/shared/shared.module";
+import {HttpClientModule} from "@angular/common/http";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {TableModule} from "primeng/table";
+import {InputMaskModule} from "primeng/inputmask";
+import {InputSwitchModule} from "primeng/inputswitch";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {DialogService, DynamicDialogModule} from "primeng/dynamicdialog";
+import {DropdownModule} from "primeng/dropdown";
+import {TooltipModule} from "primeng/tooltip";
+import {ConfirmationService} from "primeng/api";
+
+
+
+@NgModule({
+  declarations: [
+    CategoriesHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(CATEGORIES_ROTES),
+    SharedModule,
+    HttpClientModule,
+    // -----PrimeNG------
+    CardModule,
+    ButtonModule,
+    TableModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextareaModule,
+    DynamicDialogModule,
+    DropdownModule,
+    TooltipModule
+  ],
+  providers : [DialogService, ConfirmationService]
+})
+export class CategoriesModule { }
